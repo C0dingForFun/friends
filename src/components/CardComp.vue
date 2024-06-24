0aba2f4889f35824109c3c9ffd984e6a5440fcca
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-24 14:39:03 +0200

    dejavue

---
+++ src/components/CardComp.vue
@@ -0,0 +1,24 @@
+<template>
+    <slot name="make"></slot>
+    <slot name="model"></slot>
+    <slot name="year"></slot>
+    <slot name="colour"></slot>
+    <slot name="carPrice"></slot>
+
+    <slot name="name"></slot>
+    <slot name="manufacturer"></slot>
+    <slot name="release_year"></slot>
+    <slot name="storage"></slot>
+    <slot name="storage"></slot>
+    <slot name="gamePrice"></slot>
+    <slot name="best_selling_game"></slot>
+    <slot> <h1>404 No Content</h1> </slot>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
