cb77325acbf669a85214970572aa0cdcc8cbf793
C0dingForFun <151622640+C0dingForFun@users.noreply.github.com>
2024-06-25 08:22:52 +0200

    friends

--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,12 +1,21 @@
 <template>
-    <slot name="cars"></slot>
-    <slot name="consoles"></slot>
+    <div class="card">
+        <slot name="cars"></slot>
+    </div>
+    <div class="card">
+        <slot name="consoles"></slot>
+    </div>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .card{
+        margin:auto;
+        width:300px;
+        height:auto;
+        border:solid 2px green;
+    }
 </style>
